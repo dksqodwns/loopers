874ed363793bce05f3fa038ef6d2323f7a7032e3
bjahn <dksqodwns@gmail.com>
2025-07-15 01:38:08 +0900

    fix: 컨트롤러에서 birthDate타입 String으로 변경

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserDto.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserDto.java
@@ -3,10 +3,14 @@ package com.loopers.interfaces.api.user;
 import com.loopers.application.user.UserCommand.UserCreateCommand;
 import com.loopers.application.user.UserInfo;
 import com.loopers.domain.user.Gender;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Past;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class UserDto {
     public record UserResponse(Long id, String userId, String email) {
@@ -32,14 +36,23 @@ public class UserDto {
 
             @NotEmpty(message = "생년월일은 비어있을 수 없습니다.")
             @Past(message = "생년월일은 현재보다 이전이어야 합니다.")
-            LocalDate birthDate
+            String birthDate
     ) {
         public UserCreateCommand toCommand() {
+            // 여기서 birthDate을 LocalDate타입으로 바꿈
+            LocalDate parsedBirthDate;
+
+            try {
+                parsedBirthDate = LocalDate.parse(this.birthDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            } catch (DateTimeParseException e) {
+                throw new CoreException(ErrorType.BAD_REQUEST, "생년월일은 yyyy-MM-dd 형식이어야 합니다." + e.getMessage());
+            }
+
             return new UserCreateCommand(
                     this.userId,
                     this.email,
                     this.gender,
-                    this.birthDate
+                    parsedBirthDate
             );
         }
     }
