6e112f3c33e9689023add3d73460001cf04b5c63
lavarwave.byeongjun <rd_byeongjun@lavarwave.co.kr>
2025-07-16 16:03:00 +0900

    test: 회원가입 E2E 테스트 작성 완료

--- apps/commerce-api/src/main/java/com/loopers/application/user/UserInfo.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/UserInfo.java
@@ -4,11 +4,12 @@ import com.loopers.domain.user.Gender;
 import com.loopers.domain.user.User;
 import java.time.LocalDate;
 
-public record UserInfo(Long id, String userId, String email, Gender gender, LocalDate birthDate) {
+public record UserInfo(Long id, String userId, String username, String email, Gender gender, LocalDate birthDate) {
     public static UserInfo from(User user) {
         return new UserInfo(
                 user.getId(),
                 user.getUserId(),
+                user.getUsername(),
                 user.getEmail(),
                 user.getGender(),
                 user.getBirthDate()
--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/UserApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/UserApiE2ETest.java
@@ -1,7 +1,106 @@
 package com.loopers.interfaces.api;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.loopers.infrastructure.user.UserJpaRepository;
+import com.loopers.interfaces.api.ApiResponse.Metadata.Result;
+import com.loopers.interfaces.api.user.UserDto;
+import com.loopers.interfaces.api.user.UserDto.UserCreateRequest;
+import com.loopers.interfaces.api.user.UserDto.UserResponse;
+import com.loopers.utils.DatabaseCleanUp;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UserApiE2ETest {
+class UserApiE2ETest {
+
+    public static final String END_POINT = "/api/v1/users";
+
+    private final TestRestTemplate testRestTemplate;
+    private final UserJpaRepository userJpaRepository;
+    private final DatabaseCleanUp databaseCleanUp;
+
+    @Autowired
+    public UserApiE2ETest(
+            TestRestTemplate testRestTemplate,
+            UserJpaRepository userJpaRepository,
+            DatabaseCleanUp databaseCleanUp
+    ) {
+        this.testRestTemplate = testRestTemplate;
+        this.userJpaRepository = userJpaRepository;
+        this.databaseCleanUp = databaseCleanUp;
+    }
+
+    @AfterEach
+    void cleanUp() {
+        this.databaseCleanUp.truncateAllTables();
+    }
+
+    @DisplayName("POST /api/v1/users")
+    @Nested
+    class POST {
+
+        @DisplayName("회원가입 성공 시, 유저 정보를 반환한다.")
+        @Test
+        void returnUserInfo_whenUserCreated() {
+            UserDto.UserCreateRequest request = new UserCreateRequest(
+                    "test", "테스터", "test@test.com", "MALE", "1998-01-08"
+            );
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+
+            HttpEntity<UserDto.UserCreateRequest> requestEntity = new HttpEntity<>(request, headers);
+
+            ParameterizedTypeReference<ApiResponse<UserDto.UserResponse>> responseType = new ParameterizedTypeReference<>() {
+            };
+
+            ResponseEntity<ApiResponse<UserDto.UserResponse>> response = testRestTemplate.exchange(
+                    END_POINT, HttpMethod.POST, requestEntity, responseType
+            );
+
+            UserResponse responseBodyData = response.getBody().data();
+
+            assertAll(
+                    () -> assertTrue(response.getStatusCode().is2xxSuccessful()),
+                    () -> assertThat(responseBodyData.id()).isEqualTo(1L),
+                    () -> assertThat(responseBodyData.userId()).isEqualTo("test"),
+                    () -> assertThat(responseBodyData.email()).isEqualTo("test@test.com")
+            );
+        }
+
+        @DisplayName("성별이 없으면 400 에러를 반환한다.")
+        @Test
+        void return400_whenGenderIsNull() {
+            UserDto.UserCreateRequest request = new UserCreateRequest(
+                    "test", "테스터", "test@test.com", null, "1998-01-08"
+            );
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+
+            HttpEntity<UserDto.UserCreateRequest> requestEntity = new HttpEntity<>(request, headers);
+
+            ParameterizedTypeReference<ApiResponse<UserDto.UserResponse>> responseType = new ParameterizedTypeReference<>() {
+            };
+            ResponseEntity<ApiResponse<UserResponse>> response = testRestTemplate.exchange(
+                    END_POINT, HttpMethod.POST, requestEntity, responseType
+            );
+
+            assertAll(
+                    () -> assertTrue(response.getStatusCode().is4xxClientError()),
+                    () -> assertThat(response.getBody().meta().result()).isEqualTo(Result.FAIL)
+            );
+        }
+    }
 }
